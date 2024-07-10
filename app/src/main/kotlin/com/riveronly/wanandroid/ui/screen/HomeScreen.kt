f67590f63aef59746581bb68333096aba100738b
river <riveronly@outlook.com>
2024-07-10 17:47:24 +0800

    ui update

--- app/src/main/kotlin/com/riveronly/wanandroid/ui/screen/HomeScreen.kt
+++ app/src/main/kotlin/com/riveronly/wanandroid/ui/screen/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -130,6 +131,7 @@ fun HomeScreen() {
                             ?: item.shareUser)
                     }
                 )
+                HorizontalDivider()
             }
             if (pagingItems.loadState.append is LoadState.Loading) {
                 item {
--- app/src/main/kotlin/com/riveronly/wanandroid/ui/screen/PlazaScreen.kt
+++ app/src/main/kotlin/com/riveronly/wanandroid/ui/screen/PlazaScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -100,6 +101,7 @@ fun PlazaScreen() {
                             Text(text = item.shareUser)
                         }
                     )
+                    HorizontalDivider()
                 }
                 if (pagingItems.loadState.append is LoadState.Loading) {
                     item {
