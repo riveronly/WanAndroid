0fa841a8b1abe4b701982b0942e56f31fedc20fb
river <riveronly@outlook.com>
2024-07-12 16:28:23 +0800

    home tabs navHost

--- app/src/main/kotlin/com/riveronly/wanandroid/MainActivity.kt
+++ app/src/main/kotlin/com/riveronly/wanandroid/MainActivity.kt
@@ -6,17 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.riveronly.wanandroid.Tab.Home
 import com.riveronly.wanandroid.Tab.Mine
 import com.riveronly.wanandroid.Tab.Plaza
@@ -24,13 +25,11 @@ import com.riveronly.wanandroid.ui.screen.HomeScreen
 import com.riveronly.wanandroid.ui.screen.MineScreen
 import com.riveronly.wanandroid.ui.screen.PlazaScreen
 import com.riveronly.wanandroid.ui.theme.WanAndroidTheme
-import kotlinx.coroutines.launch
 
 enum class Tab(
     val title: String,
     @DrawableRes val iconResNormal: Int,
     @DrawableRes val iconResFill: Int,
-    val isUsing: Boolean = true
 ) {
     Home("首页", R.drawable.home_24px, R.drawable.home_fill_24px),
     Plaza("广场", R.drawable.dashboard_24px, R.drawable.dashboard_fill_24px),
@@ -44,49 +43,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WanAndroidTheme {
-                val pagerState = rememberPagerState(pageCount = { Tab.entries.size })
-                val scope = rememberCoroutineScope()
+                val navController = rememberNavController()
 
                 Scaffold(bottomBar = {
                     NavigationBar {
-                        Tab.entries.forEachIndexed { index, tab ->
-                            if (tab.isUsing) {
-                                val isSelected = pagerState.settledPage == index
-                                NavigationBarItem(
-                                    label = { Text(tab.title) },
-                                    selected = isSelected,
-                                    onClick = {
-                                        scope.launch {
-                                            if (!isSelected) {
-                                                pagerState.scrollToPage(index)
-                                            }
+                        Tab.entries.forEach { tab ->
+                            val isSelected =
+                                navController.currentDestination?.route == tab.title
+                            NavigationBarItem(
+                                label = { Text(tab.title) },
+                                selected = isSelected,
+                                onClick = {
+                                    navController.navigate(tab.title) {
+                                        // 避免 BackStack 增长，跳转页面时，将栈内 startDestination 之外的页面弹出
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            //出栈的 BackStack 保存状态
+                                            saveState = true
                                         }
-                                    },
-                                    icon = {
-                                        Icon(
-                                            painterResource(
-                                                if (isSelected) tab.iconResFill
-                                                else tab.iconResNormal
-                                            ),
-                                            contentDescription = ""
-                                        )
+                                        // 避免点击同一个 Item 时反复入栈
+                                        launchSingleTop = true
+
+                                        // 如果之前出栈时保存状态了，那么重新入栈时恢复状态
+                                        restoreState = true
                                     }
-                                )
-                            }
+                                },
+                                icon = {
+                                    Icon(
+                                        painterResource(
+                                            if (isSelected) tab.iconResFill
+                                            else tab.iconResNormal
+                                        ),
+                                        contentDescription = ""
+                                    )
+                                }
+                            )
                         }
                     }
                 }) { innerPadding ->
-                    HorizontalPager(
+                    NavHost(
                         modifier = Modifier.padding(innerPadding),
-                        userScrollEnabled = false,
-                        state = pagerState,
-                        beyondViewportPageCount = Tab.entries.size,
-                    ) { index ->
-                        when (Tab.entries[index]) {
-                            Home -> HomeScreen()
-                            Plaza -> PlazaScreen()
-                            Mine -> MineScreen()
-                        }
+                        navController = navController,
+                        startDestination = Home.title
+                    ) {
+                        composable(Home.title) { HomeScreen() }
+                        composable(Plaza.title) { PlazaScreen() }
+                        composable(Mine.title) { MineScreen() }
                     }
                 }
             }
