1ceb63fbd045a7278a6efc880cde2fa4c04a9071
river <riveronly@outlook.com>
2024-07-17 18:23:37 +0800

    home tabs lazyLoad

--- app/src/main/kotlin/com/riveronly/wanandroid/MainActivity.kt
+++ app/src/main/kotlin/com/riveronly/wanandroid/MainActivity.kt
@@ -6,20 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.riveronly.wanandroid.Tab.Home
 import com.riveronly.wanandroid.Tab.Mine
 import com.riveronly.wanandroid.Tab.Plaza
@@ -27,6 +26,7 @@ import com.riveronly.wanandroid.ui.screen.HomeScreen
 import com.riveronly.wanandroid.ui.screen.MineScreen
 import com.riveronly.wanandroid.ui.screen.PlazaScreen
 import com.riveronly.wanandroid.ui.theme.WanAndroidTheme
+import kotlinx.coroutines.launch
 
 enum class Tab(
     val title: String,
@@ -46,60 +46,61 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             WanAndroidTheme {
-                val navController = rememberNavController()
+                val pagerState = rememberPagerState(
+                    initialPage = 2,
+                    pageCount = { Tab.entries.size }
+                )
+                val scope = rememberCoroutineScope()
+                val currentPagerIndex = pagerState.currentPage
+                val isInitList =
+                    rememberSaveable {
+                        mutableStateOf(BooleanArray(Tab.entries.size) { index -> index == currentPagerIndex })
+                    }
+
                 Scaffold(
                     bottomBar = {
-                        CustomizeNavigationBar(navController)
+                        NavigationBar {
+                            Tab.entries.forEachIndexed { index, tab ->
+                                val isSelected = currentPagerIndex == index
+                                NavigationBarItem(
+                                    label = { Text(tab.title) },
+                                    selected = isSelected,
+                                    onClick = {
+                                        scope.launch {
+                                            isInitList.value[index] = true
+                                            pagerState.scrollToPage(index)
+                                        }
+                                    },
+                                    icon = {
+                                        Icon(
+                                            painterResource(
+                                                if (isSelected) tab.iconResFill
+                                                else tab.iconResNormal
+                                            ),
+                                            contentDescription = ""
+                                        )
+                                    }
+                                )
+                            }
+                        }
                     }
                 ) { innerPadding ->
-                    NavHost(
+                    HorizontalPager(
                         modifier = Modifier.padding(innerPadding),
-                        navController = navController,
-                        startDestination = Home.title
-                    ) {
-                        composable(Home.title) { HomeScreen() }
-                        composable(Plaza.title) { PlazaScreen() }
-                        composable(Mine.title) { MineScreen() }
+                        userScrollEnabled = false,
+                        state = pagerState,
+                        beyondViewportPageCount = Tab.entries.size,
+                    ) { index ->
+                        if (isInitList.value[index]) {
+                            when (Tab.entries[index]) {
+                                Home -> HomeScreen()
+                                Plaza -> PlazaScreen()
+                                Mine -> MineScreen()
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun CustomizeNavigationBar(navController: NavController) {
-    NavigationBar {
-        Tab.entries.forEach { tab ->
-            val isSelected =
-                navController.currentDestination?.route == tab.title
-            NavigationBarItem(
-                label = { Text(tab.title) },
-                selected = isSelected,
-                onClick = {
-                    navController.navigate(tab.title) {
-                        // 避免 BackStack 增长，跳转页面时，将栈内 startDestination 之外的页面弹出
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            //出栈的 BackStack 保存状态
-                            saveState = true
-                        }
-                        // 避免点击同一个 Item 时反复入栈
-                        launchSingleTop = true
-
-                        // 如果之前出栈时保存状态了，那么重新入栈时恢复状态
-                        restoreState = true
-                    }
-                },
-                icon = {
-                    Icon(
-                        painterResource(
-                            if (isSelected) tab.iconResFill
-                            else tab.iconResNormal
-                        ),
-                        contentDescription = ""
-                    )
-                }
-            )
         }
     }
 }
